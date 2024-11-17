c77c1a42203c8d55fb7fd8353bfe70a3b3992128
kandysh <rajawat.k.029@gmail.com>
2024-11-17 18:04:11 +0530

    sol

--- src/day3/MazeSolver.ts
+++ src/day3/MazeSolver.ts
@@ -1,49 +1,50 @@
 const dir = [
-        [0, -1],
+        [-1, 0],
         [1, 0],
-        [0, 1],
-        [-1, 0]
-];
+        [0, -1],
+        [0, 1]];
 function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
+        // base case
+
         // off the map
         if (curr.x < 0 || curr.x >= maze[0].length || curr.y < 0 || curr.y >= maze.length) {
                 return false;
         }
-        // at a wall
-        if (maze[curr.x][curr.y] === wall) {
+
+        // on the wall
+        if (maze[curr.y][curr.x] === wall)
                 return false;
 
-        }
-        // at the end
-        if (curr.x === end.x && curr.y == end.y) {
+        // end
+        if (curr.x === end.x && curr.y === end.y) {
+                path.push(end);
                 return true;
         }
-        // already seen
-        if (seen[curr.x][curr.y]) {
+        if (seen[curr.y][curr.x])
                 return false;
-        }
 
-        // recursive case
+        // recursive cases
 
         // pre
+        seen[curr.y][curr.x] = true;
         path.push(curr);
-        // recursive
 
         for (let i = 0; i < dir.length; ++i) {
                 const [x, y] = dir[i];
-                if (walk(maze, wall, {
-                        x: curr.x + x,
-                        y: curr.y + y,
-                }, end, seen, path)) {
-                        return true
-                }
+                if (walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path))
+                        return true;
         }
+
         // post
         path.pop();
-
+        return false;
 }
-
-
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
-
+        const seen: boolean[][] = [];
+        const path: Point[] = [];
+        for (let i = 0; i < maze.length; ++i) {
+                seen.push(new Array(maze[0].length).fill(false));
+        }
+        walk(maze, wall, start, end, seen, path);
+        return path;
 }
---
+++ src/day4/QuickSort.ts
@@ -0,0 +1,33 @@
+function qs(arr: number[], lo: number, hi: number): void {
+        if (lo >= hi)
+                return;
+
+        const pi = partition(arr, lo, hi);
+        qs(arr, lo, pi - 1);
+        qs(arr, pi + 1, hi);
+}
+function partition(arr: number[], lo: number, hi: number): number {
+        // hi is the pivot
+        let pivot = arr[hi];
+        let idx = lo - 1;
+
+        for (let i = lo; i < hi; ++i) {
+                if (arr[i] <= pivot) {
+                        idx++;
+                        let temp = arr[i];
+                        arr[i] = arr[idx];
+                        arr[idx] = temp;
+                }
+
+        }
+        idx++;
+        arr[hi] = arr[idx];
+        arr[idx] = pivot;
+
+        return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+        qs(arr, 0, arr.length - 1);
+
+}
